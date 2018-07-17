2df098d1b7525f00d578011e6f48348f6cf3bd5d
GORosin <oxr3231@g.rit.edu>
2018-07-17 16:04:23 -0400

    routes added for every button with input values returned

--- GUIroute.py
+++ GUIroute.py
@@ -6,8 +6,27 @@ app = Flask(__name__)
 def my_form():
     return render_template('my-form.html')
 
+
+@app.route('/low', methods=['GET','POST'])
+def low_voltage():
+    if request.method == 'POST':
+        result = request.form['low']
+        print(result)
+        return render_template('my-form.html')
+
+@app.route('/high', methods=['GET','POST'])
+def high_voltage():
+    if request.method == 'POST':
+        result = request.form['high']
+        print(result)
+        return render_template('my-form.html')
+
+@app.route('/IVscan', methods=['GET','POST'])
+def iv_scan():
+    pass
+
 @app.route('/temp', methods=['GET','POST'])
-def test_post():
+def temp():
     if request.method == 'POST':
         result = request.form['temperature']
         print(result)
@@ -20,10 +39,40 @@ def ping():
         print(ping)
         return render_template('my-form.html')
 
+@app.route('/sensor', methods=['GET','POST'])
+def sensor_data():
+    if request.method == 'POST':
+        sensor = request.form['sensor']
+        print(sensor)
+        return render_template('my-form.html')
+
+
 @app.route('/break', methods=['GET','POST'])
-def breakConnection():
+def break_connection():
     pass
 
+@app.route('/confirmtest', methods=['GET','POST'])
+def confirmation_test():
+    if request.method == 'POST':
+        result = request.form['test']
+        print(result)
+        return render_template('my-form.html')
+
+@app.route('/interlock', methods=['GET','POST'])
+def set_interlock():
+    if request.method == 'POST':
+        temp_before = request.form['temp before']
+        temp_after = request.form['temp after']
+        humidity_before = request.form['humidity before']
+        humidity_after = request.form['humidity after']
+        hybrid_before = request.form['hybrid before']
+        hybrid_after = request.form['hybrid after']
+        user_before = request.form['user before']
+        user_after = request.form['user after']
+
+        result = temp_before,temp_after,humidity_before,humidity_after,hybrid_before,hybrid_after,user_before,user_after
+        print(result)
+        return render_template('my-form.html')
 
 
 if __name__ == "__main__":
