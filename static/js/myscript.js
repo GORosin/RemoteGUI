0da8b4dc51d5ed1e3dfe8112e546191261d80943
GORosin <oxr3231@g.rit.edu>
2018-07-17 23:38:20 -0400

    jquery content moved back to myscript.js

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,3 +1,123 @@
+    $(function() {
+        $(document.getElementById("read back low")).click(function() {
+            $.ajax({
+                url: '/low',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("read back high")).click(function() {
+            $.ajax({
+                url: '/high',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("SetTemp")).click(function() {
+            $.ajax({
+                url: '/temp',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("SetPing")).click(function() {
+            $.ajax({
+                url: '/ping',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("Break")).click(function() {
+            $.ajax({
+                url: '/break',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("IV scan")).click(function() {
+            $.ajax({
+                url: '/IVscan',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("Sensor")).click(function() {
+            $.ajax({
+                url: '/sensor',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("Run Confirmation Test")).click(function() {
+            $.ajax({
+                url: '/confirmtest',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+        $(document.getElementById("Set Interlock")).click(function() {
+            $.ajax({
+                url: '/interlock',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+    });
+
 function noReload(){
 	if(event.key == 'Enter'){
 		event.preventDefault();
